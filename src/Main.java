79472b4d019de2cf4315424326797a853a6ae473
tkesici <20200808004@ogr.akdeniz.edu.tr>
2022-06-19 21:39:11 +0300

    Algorithm replacement

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,33 @@ public class Main {
 
     public static void main(String[] args) {
         int[] A = {4, 3, 1, 2, 5};
-        // System.out.println("Result: " + smallest(A));
-        System.out.println("Array: " + Arrays.toString(selectionSort(A)));
-    }
-
-    public static int smallest(int[] A) {
-        int smallest = A[0];
-        for (int i = 1; i < A.length; i++) {
-            if (A[i] < smallest) {
-                smallest = A[i];
-                A[i] = A[i - 1];
-                A[i - 1] = smallest;
-            }
-        }
-        return smallest;
+        System.out.println("Done! ->    " + Arrays.toString(selectionSort(A)));
     }
 
     public static int[] selectionSort(int[] A) {
-        int key;
-        int smallest;
         for (int i = 0; i < A.length - 1; i++) {
-            key = A[i];
-            smallest = A[i + 1];
-            System.out.println("[---START---]");
-            System.out.println("Array: " + Arrays.toString(A));
-            System.out.println("Key: " + key);
+            int smallest = 2147483647;
+            int position = 0;
+            int temp = 0;
+            System.out.println("A[i] = " + i + " -> " + Arrays.toString(A));
             for (int j = i + 1; j < A.length; j++) {
-                if (A[i] > A[j]) {
-                    if (A[j] < smallest) {
+                if ((A[j] < smallest)) {
+                    smallest = A[j];
+                    position = j;
+                }
+                if (j == A.length - 1 && i == A.length - 2) {
+                    if (A[i] > A[j]) {
                         smallest = A[j];
-                        A[j] = key;
-                        A[i] = smallest;
+                        position = j;
+                    } else {
+                        smallest = A[i];
+                        position = i;
                     }
                 }
             }
-            System.out.println("To replace: " + smallest);
-            System.out.println("[---END---]\n");
+            temp = A[i];
+            A[i] = smallest;
+            A[position] = temp;
         }
         return A;
     }
