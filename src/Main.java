83bf685672c8f690cab4140d57606c9ce75e4874
tkesici <20200808004@ogr.akdeniz.edu.tr>
2022-06-20 03:46:26 +0300

    Bug fix

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,23 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] A = {4, 3, 1, 2, 5};
-        System.out.println("Done! ->    " + Arrays.toString(selectionSort(A)));
+        int[] A = {4, 30, 101, 6, 45, 67, 7, 1, 2, 5, 99, 111, 34, 3};
+        System.out.println("Done!     -> " + Arrays.toString(selectionSort(A)));
     }
 
     public static int[] selectionSort(int[] A) {
+
         for (int i = 0; i < A.length - 1; i++) {
-            int smallest = 2147483647;
-            int position = 0;
-            int temp = 0;
-            System.out.println("A[i] = " + i + " -> " + Arrays.toString(A));
+            int smallest = i;
             for (int j = i + 1; j < A.length; j++) {
-                if ((A[j] < smallest)) {
-                    smallest = A[j];
-                    position = j;
-                }
-                if (j == A.length - 1 && i == A.length - 2) {
-                    if (A[i] > A[j]) {
-                        smallest = A[j];
-                        position = j;
-                    } else {
-                        smallest = A[i];
-                        position = i;
-                    }
-                }
+                if ((A[j] < A[smallest])) smallest = j;
             }
-            temp = A[i];
-            A[i] = smallest;
-            A[position] = temp;
+            System.out.println("Step " + (i + 1) + "   -> "
+                    + Arrays.toString((A)));
+            int temp = A[smallest];
+            A[smallest] = A[i];
+            A[i] = temp;
+
         }
         return A;
     }
